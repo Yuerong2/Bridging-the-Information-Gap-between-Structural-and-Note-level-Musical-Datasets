a98555d4e997d36a298bf323b91568ea47ebc6c9
Yuerong2 <31352125+Yuerong2@users.noreply.github.com>
2018-12-14 11:04:32 -0600

    Update Generate Billboard Triples.py

--- python scripts/Generate Billboard Triples.py
+++ python scripts/Generate Billboard Triples.py
@@ -2,19 +2,50 @@ import csv
 import re
 from urllib.parse import quote
 #fileout = ("BillboardTriples.txt", 'w')
-with open('/Users/hu/Desktop/Index.csv', 'r') as db:
+with open('index.csv', 'r') as db:
     reader = csv.DictReader(db)
     tripleList = "list of triples"
     for row in reader:
         # print(type(row['title'])):class str
         if not row['artist'] == '':
-            name = ''.join(row['title'].split())
-            artistUrl = quote(row['artist'], safe='')
-            print ("<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   foaf:maker",
-                         "<http://billboard.linkedmusic.org/track/" + row['id'] + ">" + '.'+"\n"
+            p = re.compile('"')
+            findarr = p.findall(row['artist'])
+            if len(findarr)== 0:
+               findarr2=p.findall(row['title'])
+               if len(findarr2)==0:
+                   name = ''.join(row['title'].split())
+                   artistUrl = quote(row['artist'], safe='')
+                   print("<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   foaf:maker",
+                         "<http://billboard.linkedmusic.org/track/" + row['id'] + ">" + '.' + "\n"
                          + "<http://billboard.linkedmusic.org/track/" + row['id'] + ">", "   skos:prefLabel  ",
-                         '"' + row['title'] + '"'+'.'+"\n"
-                         +"<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   skos:prefLabel  ",
-                         '"' + row['artist'] + '"' +'.'+ "\n")
-
-#fileout.close()
+                         '"' + row['title'] + '"' + '.' + "\n"
+                         + "<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   skos:prefLabel  ",
+                         '"' + row['artist'] + '"' + '.' + "\n")
+               else:
+                   name = ''.join(row['title'].split())
+                   artistUrl = quote(row['artist'], safe='')
+                   print("<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   foaf:maker",
+                         "<http://billboard.linkedmusic.org/track/" + row['id'] + ">" + '.' + "\n"
+                         + "<http://billboard.linkedmusic.org/track/" + row['id'] + ">", "   skos:prefLabel  ",
+                         '\'' + row['title'] + '\'' + '.' + "\n"
+                         + "<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   skos:prefLabel  ",
+                         '"' + row['artist'] + '"' + '.' + "\n")
+            else:
+                if len(findarr2) == 0:
+                    name = ''.join(row['title'].split())
+                    artistUrl = quote(row['artist'], safe='')
+                    print("<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   foaf:maker",
+                          "<http://billboard.linkedmusic.org/track/" + row['id'] + ">" + '.' + "\n"
+                          + "<http://billboard.linkedmusic.org/track/" + row['id'] + ">", "   skos:prefLabel  ",
+                          '"' + row['title'] + '"' + '.' + "\n"
+                          + "<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   skos:prefLabel  ",
+                          '\'' + row['artist'] + '\'' + '.' + "\n")
+                else:
+                    name = ''.join(row['title'].split())
+                    artistUrl = quote(row['artist'], safe='')
+                    print("<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   foaf:maker",
+                          "<http://billboard.linkedmusic.org/track/" + row['id'] + ">" + '.' + "\n"
+                          + "<http://billboard.linkedmusic.org/track/" + row['id'] + ">", "   skos:prefLabel  ",
+                          '\'' + row['title'] + '\'' + '.' + "\n"
+                          + "<http://billboard.linkedmusic.org/artist/" + artistUrl + ">", "   skos:prefLabel  ",
+                          '\'' + row['artist'] + '\'' + '.' + "\n")
